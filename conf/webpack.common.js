92eb630d37fac45df6f498fe0bd11d96d54b996e
Robby <ta7382@gmail.com>
2018-05-23 16:21:08 +0800

    Update webpack

--- conf/webpack.common.js
+++ conf/webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
@@ -21,7 +22,7 @@ module.exports = {
   output: {
     path: path.join(process.cwd(), 'dist'),
     filename: '[name].[hash].js',
-    publicPath: '/',
+    publicPath: './',
     sourceMapFilename: '[name].map'
   },
 
@@ -77,6 +78,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'index.html'
     }),
+    new CopyWebpackPlugin([{
+      from: 'assets',
+      to: 'assets'
+    }]),
     extractSass
   ]
 };
